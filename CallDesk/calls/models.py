51cf3f762020ed5801a828d2cbb7553d74b0d6f1
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-12-16 09:32:35 +0300

    Category registry

--- CallDesk/calls/models.py
+++ CallDesk/calls/models.py
@@ -78,3 +78,13 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
