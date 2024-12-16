408ebb622e701c39ee3d411eed5eb5f952a7ce2b
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-12-16 09:33:14 +0300

    Resolution

--- CallDesk/calls/models.py
+++ CallDesk/calls/models.py
@@ -88,3 +88,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class ActionTaken(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
