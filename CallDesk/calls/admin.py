84713ec86c512aa5d29dbb14f5660c82edeb8d41
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-12-16 09:39:45 +0300

    models

--- CallDesk/calls/admin.py
+++ CallDesk/calls/admin.py
@@ -1,6 +1,11 @@
 # calls/admin.py
 from django.contrib import admin
-from .models import Customer, CallLog
+from .models import Customer, User, Category, ActionTaken, Recommendation, CallLog, Report
 
 admin.site.register(Customer)
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(ActionTaken)
+admin.site.register(Recommendation)
 admin.site.register(CallLog)
+admin.site.register(Report)
