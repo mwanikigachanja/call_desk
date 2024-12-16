3a1f57ead37a7f429a93ef6bdb4376c243b04497
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-12-16 09:27:58 +0300

    Customer

--- CallDesk/calls/models.py
+++ CallDesk/calls/models.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import AbstractUser, User
 
 # Customer Model
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     phone = models.CharField(max_length=15, unique=True)
     email = models.EmailField(blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
